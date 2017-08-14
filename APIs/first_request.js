bf7537b0b18c33f85548fda9c44bd243d69c1054
andrew-rayco <andy@andycondon.com>
2017-08-14 15:26:57 +1200

    add super simple http request with request package

---
+++ APIs/first_request.js
@@ -0,0 +1,11 @@
+var request = require('request')
+request('http://www.google.com/', (error, response, body) => {
+  if (error) {
+    console.log('Something went wrong!')
+    console.log(error)
+  } else {
+    if (response.statusCode == 200) {
+      console.log(body)
+    }
+  }
+})
