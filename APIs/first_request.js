d0c8a78f6475cb9ed8d1bb7275f199965c39be0b
andrew-rayco <andy@andycondon.com>
2017-08-14 15:41:40 +1200

    api done

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,11 +1,16 @@
 var request = require('request')
-request('http://www.google.com/', (error, response, body) => {
-  if (error) {
-    console.log('Something went wrong!')
-    console.log(error)
-  } else {
+
+console.log('My favourite is:')
+request('http://ergast.com/api/f1/2017/5/driverStandings.json', (error, response, body) => {
     if (response.statusCode == 200) {
-      console.log(body)
+      var parsedData = JSON.parse(body)
+      console.log(parsedData.MRData.StandingsTable.StandingsLists[0].DriverStandings[4].Driver.familyName)
     }
-  }
 })
+
+
+// General search:
+// http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb
+//
+// Search with Movie ID:
+// http://www.omdbapi.com/?i=tt3896198&apikey=thewdb 
