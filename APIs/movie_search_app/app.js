8e87d1e7f503b30214202b42e856615066e4c298
andrew-rayco <andy@andycondon.com>
2017-08-14 16:18:00 +1200

    search results based on req.query

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -5,8 +5,15 @@ var app = express()
 
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => {
+  res.render('search')
+})
+
 app.get('/results', (req, res) => {
-  request('http://www.omdbapi.com/?s=zealand&apikey=thewdb', (error, response, body) => {
+  var query = req.query.search
+  var url = 'http://www.omdbapi.com/?s='
+  var key = '&apikey=thewdb'
+  request(url + query + key, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body)
       res.render('results', {data: data})
