f638635aca23ef67df690e41d7143ef5eb304b94
andrew-rayco <andy@andycondon.com>
2017-08-14 12:50:52 +1200

    adding POST route req.body to data array and redirecting back to list

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set ('view engine', 'ejs')
+
+var friends = ['Tony', 'Brett', 'Micky D', 'Simon', 'Scotty']
 
 app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.post('/addfriend', (req, res) => {
+  var newfriend = req.body.newFriend
+  friends.push(newfriend)
+  res.redirect('/friends')
+})
+
 app.get('/friends', (req, res) => {
-  var friends = ['Tony', 'Brett', 'Micky D', 'Simon', 'Scotty']
   res.render('friends', {friends})
 })
 
