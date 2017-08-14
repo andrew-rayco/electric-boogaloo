4e76e989ec125e9b43c28d2c27a1c5d5935246cf
andrew-rayco <andy@andycondon.com>
2017-08-14 16:08:45 +1200

    viewing api response in ejs view

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -3,8 +3,15 @@ var request = require('request')
 
 var app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/results', (req, res) => {
-  res.send('you found me')
+  request('http://www.omdbapi.com/?s=zealand&apikey=thewdb', (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body)
+      res.render('results', {data: data})
+    }
+  })
 })
 
 var PORT = 3000
