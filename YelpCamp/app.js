2df0c4a5eca781c0535d17f3c81d55380e51dbab
andrew-rayco <andy@andycondon.com>
2017-08-14 16:56:17 +1200

    add form

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,19 +1,32 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
+
+var campgrounds = [
+  { name: "Salmon Creek", image: "http://photosforclass.com/download/2123340163" },
+  { name: "Granite Hill", image: "http://photosforclass.com/download/7121863467" },
+  { name: "Mountain Goat's Rest", image: "http://photosforclass.com/download/2602356334" }
+]
 
 app.get('/', (req, res) => {
   res.render('landing')
 })
 
 app.get('/campgrounds', (req, res) => {
-  var campgrounds = [
-    { name: "Salmon Creek", image: "http://photosforclass.com/download/2123340163" },
-    { name: "Granite Hill", image: "http://photosforclass.com/download/7121863467" },
-    { name: "Mountain Goat's Rest", image: "http://photosforclass.com/download/2602356334" }
-  ]
   res.render('campgrounds', { campgrounds })
+})
+
+app.post('/campgrounds', (req, res) => {
+  // get data from form and add too campgrounds array
+  // redirect back to campgrounds page
+  res.send('hello i am here')
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new')
 })
 
 var PORT = 3000
