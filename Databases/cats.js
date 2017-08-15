12222cb51892847a05316ed193a6f834c01bd714
andrew-rayco <andy@andycondon.com>
2017-08-15 16:10:06 +1200

    find all

--- Databases/cats.js
+++ Databases/cats.js
@@ -14,19 +14,29 @@ var Cat = mongoose.model("Cat", catSchema)
 
 // adding a cat to the db
 
-var george = new Cat({
-  name: "George",
-  age: 11,
-  temperament: "Grouchy"
-})
-
-george.save((err, cat) => {
-  if (err) {
-    console.log('Something went wrong. Soz.')
-  } else {
-    console.log("We just saved a cat to the database!")
-    console.log(cat)
-  }
-})
+// var george = new Cat({
+//   name: "Mrs Norris",
+//   age: 7,
+//   temperament: "Evil"
+// })
+//
+// george.save((err, cat) => {
+//   if (err) {
+//     console.log('Something went wrong. Soz.')
+//   } else {
+//     console.log("We just saved a cat to the database!")
+//     console.log(cat)
+//   }
+// })
 
 // retrieve all cats from the db and console.log each one
+
+Cat.find({}, (err, cats) => {
+  if(err) {
+    console.log('Oh no, error!');
+    console.log(err);
+  } else {
+    console.log('All the cats');
+    console.log(cats);
+  }
+})
