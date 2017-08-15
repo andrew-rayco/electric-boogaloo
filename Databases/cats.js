6015c64d8e49e3abe49a0b4fa49b22b32a200875
andrew-rayco <andy@andycondon.com>
2017-08-15 16:03:58 +1200

    add to database

--- Databases/cats.js
+++ Databases/cats.js
@@ -1,5 +1,32 @@
-car mongoose = require('mongoose')
+var mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost/cat_app", {useMongoClient: true})
+
+mongoose.Promise = global.Promise
+
+var catSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  temperament: String
+})
+
+var Cat = mongoose.model("Cat", catSchema)
+
 
 // adding a cat to the db
+
+var george = new Cat({
+  name: "George",
+  age: 11,
+  temperament: "Grouchy"
+})
+
+george.save((err, cat) => {
+  if (err) {
+    console.log('Something went wrong. Soz.')
+  } else {
+    console.log("We just saved a cat to the database!")
+    console.log(cat)
+  }
+})
 
 // retrieve all cats from the db and console.log each one
