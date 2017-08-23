165eb97627f02e641495518657c0ba4e60996e61
andrew-rayco <andy@andycondon.com>
2017-08-24 11:36:58 +1200

    271 completed

---
+++ Associations/references.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/blog-demo_2', {useMongoClient: true})
+
+mongoose.Promise = global.Promise
+
+// POST - title, content
+var postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+var Post = mongoose.model('Post', postSchema)
+
+// USER - email, name
+var userSchema = new mongoose.Schema({
+  email: String,
+  name: String,
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ]
+})
+var User = mongoose.model('User', userSchema)
+
+// Post.create({
+//   title: "How to cook the best burger, Part 3",
+//   content: "asdgfasdfgadsfg adsfgadfg"
+// }, (err, post) => {
+//   User.findOne({email: 'bob@gmail.com'}, (err, foundUser) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       foundUser.posts.push(post)
+//       foundUser.save((err, data) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log(data);
+//         }
+//       })
+//     }
+//   })
+// })
+
+User.findOne({email: 'bob@gmail.com'}).populate('posts').exec((err, user) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(user);
+  }
+})
+
+
+
+
+// User.create({
+//   email: 'bob@gmail.com',
+//   name: 'Bob Belcher'
+// })
