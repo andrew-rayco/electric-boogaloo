d428e1dfe90c1a57e99869be59e219a454957e4e
andrew-rayco <andy@andycondon.com>
2017-08-24 15:27:02 +1200

    274 complete

--- YelpCamp/seeds.js
+++ YelpCamp/seeds.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose')
 var Campground = require('./models/campground')
+var Comment = require('./models/comment')
 
 var data = [
   {
@@ -28,16 +29,26 @@ function seedDB() {
     console.log('removed all campgrounds');
     // add a few campgrounds
     data.forEach((seed) => {
-      Campground.create(seed, (err, data) => {
+      Campground.create(seed, (err, campground) => {
         if (err) {
           console.log(err);
         }
         console.log('== Added a new campground ==');
-        console.log(data);
+        // add a few comments
+        Comment.create({
+          text: "This place is great but I wish there was internet",
+          author: "Homer"
+        }, (err, comment) => {
+          if (err) {
+            console.log(err);
+          }
+          campground.comments.push(comment)
+          campground.save()
+          console.log('Created new comment');
+        })
       })
     })
   })
-  // add a few comments
 }
 
 module.exports = seedDB
