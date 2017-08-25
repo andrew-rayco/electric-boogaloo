7da273c8de7bf8bc40092d5b21f343c58de4ac8a
Andrew <andy@andycondon.com>
2017-08-25 12:03:33 +1200

    create user

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -8,12 +8,14 @@ var express               = require('express'),
 var User                  = require('./models/user')
 
 mongoose.connect('mongodb://localhost/auth_demo_app', {useMongoClient: true})
+mongoose.Promise = global.Promise
 
 var app = express()
 
 
 app.set('view engine', 'ejs')
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(require('express-session')({
   secret: "Be hearty in your approbation and lavish in your praise",
   resave: false,
@@ -25,7 +27,9 @@ app.use(passport.session())
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-// Routes
+// ===================
+// ROUTES
+// ===================
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -33,6 +37,26 @@ app.get('/', (req, res) => {
 
 app.get('/secret', (req, res) => {
   res.render('secret')
+})
+
+// Auth routes
+
+// Show signup form
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// Handle user signup
+app.post('/register', (req, res) => {
+  User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+    if (err) {
+      console.log(err)
+      res.render('register')
+    }
+    passport.authenticate('local')(req, res, () => {
+      res.redirect('/secret')
+    })
+  })
 })
 
 var PORT = 3000
