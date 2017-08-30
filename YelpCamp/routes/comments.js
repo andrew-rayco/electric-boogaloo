79eee44e1a03bc2fd5a1402d01a1836f934eedf1
andrew-rayco <andy@andycondon.com>
2017-08-31 05:39:28 +1200

    302 complete

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -46,6 +46,30 @@ router.post('/', isLoggedIn, (req, res) => {
   })
 })
 
+// Comments edit route
+router.get('/:comment_id/edit', (req, res) => {
+  Comment.findById(req.params.comment_id, (err, foundComment) => {
+    if (err) {
+      console.log(err)
+      res.redirect('back')
+    } else {
+      res.render('comments/edit', {campground_id: req.params.id, comment: foundComment})
+    }
+  })
+})
+
+// Comments update route
+router.put('/:comment_id', (req, res) => {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+    if (err) {
+      console.log(err)
+      res.redirect('back')
+    } else {
+      res.redirect('/campgrounds/' + req.params.id)
+    }
+  })
+})
+
 // Middleware
 // Authentication check function
 function isLoggedIn(req, res, next) {
