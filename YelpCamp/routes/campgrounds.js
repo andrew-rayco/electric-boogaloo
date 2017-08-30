26ce944d4ace551108c6e5413631e07c03bf8cd1
andrew-rayco <andy@andycondon.com>
2017-08-30 16:33:12 +1200

    solve issue with undeclared username

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -18,28 +18,34 @@ router.get('/', (req, res) => {
 })
 
 // CREATE - add new campground to DB
-router.post('/', (req, res) => {
+router.post('/', isLoggedIn, (req, res) => {
   var name = req.body.name
   var image = req.body.image
   var description = req.body.description
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  }
   var newCampground = {
     name,
     image,
-    description
+    description,
+    author
   }
-  Campground.create(newCampground, (err, res) => {
+  console.log(newCampground)
+  Campground.create(newCampground, (err, newlyCreated) => {
     if (err) {
       console.log(err);
     } else {
       console.log('Woop, created a new campground');
-      console.log(res);
+      console.log(newlyCreated);
     }
   })
   res.redirect('/campgrounds')
 })
 
 // NEW - show form to create new campground
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
   res.render('campgrounds/new')
 })
 
@@ -54,5 +60,13 @@ router.get('/:id', (req, res) => {
     }
   })
 })
+
+// Authentication check function
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
+}
 
 module.exports = router
