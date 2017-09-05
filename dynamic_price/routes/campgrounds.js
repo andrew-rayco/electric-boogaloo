bc58a9bd37169a3fb4e6a56cb9629626e2e297ea
andrew-rayco <andy@andycondon.com>
2017-09-05 15:31:00 +1200

    add dynamic price to route and show view

--- dynamic_price/routes/campgrounds.js
+++ dynamic_price/routes/campgrounds.js
@@ -24,6 +24,7 @@ router.post('/', middleware.isLoggedIn, (req, res) => {
   var name = req.body.name
   var image = req.body.image
   var description = req.body.description
+  var price = req.body.price
   var author = {
     id: req.user._id,
     username: req.user.username
@@ -32,6 +33,7 @@ router.post('/', middleware.isLoggedIn, (req, res) => {
     name,
     image,
     description,
+    price,
     author
   }
   console.log(newCampground)
