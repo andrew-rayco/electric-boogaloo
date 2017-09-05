5634ee13ac46333889e8aa819ddd3a66c263bc56
andrew-rayco <andy@andycondon.com>
2017-09-05 15:12:07 +1200

    duplicate YelpCamp into dynamic_price

---
+++ dynamic_price/app.js
@@ -0,0 +1,53 @@
+var express         = require('express'),
+    bodyParser      = require('body-parser'),
+    mongoose        = require('mongoose'),
+    passport        = require('passport'),
+    LocalStrategy   = require('passport-local'),
+    methodOverride  = require('method-override'),
+    flash           = require('connect-flash')
+
+var Campground = require('./models/campground'),
+    Comment    = require('./models/comment'),
+    User       = require('./models/user'),
+    seedDB     = require('./seeds')
+
+var campgroundRoutes = require('./routes/campgrounds'),
+    commentRoutes    = require('./routes/comments'),
+    authRoutes       = require('./routes/auth')
+
+var app = express()
+
+mongoose.Promise = global.Promise //mpromise is deprecated, use global instead
+mongoose.connect('mongodb://localhost/yelp_camp', {useMongoClient: true})
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'))
+app.use(methodOverride('_method'))
+app.set('view engine', 'ejs')
+app.use(flash())
+// seedDB() // seed the database
+
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({secret: "Be hearty in your approbation and lavish in your praise.", resave: false, saveUninitialized: false}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+// Middleware to make user object available to all views (for nav header login/out)
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user
+  res.locals.error = req.flash('error')
+  res.locals.success = req.flash('success')
+  next()
+})
+
+// Routes
+app.use(authRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/comments', commentRoutes)
+
+var PORT = 3000
+app.listen(process.env.PORT || PORT, () => {
+  console.log('YelpCamp is alive on port', PORT)
+})
---
+++ dynamic_price/seeds.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose')
+var Campground = require('./models/campground')
+var Comment = require('./models/comment')
+
+var data = [
+  {
+    name: "Cloud's Rest",
+    image: "https://farm5.staticflickr.com/4376/36437924985_07bb927043.jpg",
+    description: "Gets pretty hot out here on some days. Cold at nights. Pretty though. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus aut odit accusamus ab cupiditate! Harum, in. Fuga, obcaecati, sed! Provident ipsa labore vel ullam magni doloribus quae, beatae, culpa alias?"
+  }, {
+    name: "Boney Potanga",
+    image: "https://farm5.staticflickr.com/4176/34533122526_13d698e62a.jpg",
+    description: "Weird name. The place is cool as shit though. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, similique? Iste repellat, aspernatur sapiente ratione! Minus perspiciatis est in quam laboriosam enim, laudantium incidunt esse itaque provident, magni, blanditiis aperiam."
+  }, {
+    name: "Dove's Pier",
+    image: "https://farm8.staticflickr.com/7457/9586944536_9c61259490.jpg",
+    description: "Weird that there's no doves here. Or a pier for that matter. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis dicta id voluptas, corrupti iste, dolor vel molestiae numquam obcaecati libero sapiente, doloremque quis modi voluptatibus repellat rerum error! Doloribus, quasi."
+  }
+]
+
+function seedDB() {
+  // Remove all campgrounds
+  Campground.remove({}, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('removed all campgrounds');
+    // add a few campgrounds
+    data.forEach((seed) => {
+      Campground.create(seed, (err, campground) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('== Added a new campground ==');
+        // add a few comments
+        Comment.create({
+          text: "This place is great but I wish there was internet",
+          author: "Homer"
+        }, (err, comment) => {
+          if (err) {
+            console.log(err);
+          }
+          campground.comments.push(comment)
+          campground.save()
+          console.log('Created new comment');
+        })
+      })
+    })
+  })
+}
+
+module.exports = seedDB
