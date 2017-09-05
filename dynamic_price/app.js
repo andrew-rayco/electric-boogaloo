df4c37aeb5cde86fd1c0dac7785c2b06d1b63955
andrew-rayco <andy@andycondon.com>
2017-09-05 15:24:03 +1200

    add price to new and edit form

--- dynamic_price/app.js
+++ dynamic_price/app.js
@@ -18,7 +18,7 @@ var campgroundRoutes = require('./routes/campgrounds'),
 var app = express()
 
 mongoose.Promise = global.Promise //mpromise is deprecated, use global instead
-mongoose.connect('mongodb://localhost/yelp_camp', {useMongoClient: true})
+mongoose.connect('mongodb://localhost/yelp_camp_dyn_price', {useMongoClient: true})
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'))
 app.use(methodOverride('_method'))
